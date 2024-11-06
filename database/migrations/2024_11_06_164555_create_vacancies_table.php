5ef5307c6838bd6aeed39f07c684ea06ce75eff7
gabrielrcosta1 <contatogrsoftwares@gmail.com>
2024-11-06 12:51:22 -0400

    style: apply Laravel Pint formatting

--- database/migrations/2024_11_06_164555_create_vacancies_table.php
+++ database/migrations/2024_11_06_164555_create_vacancies_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,16 +14,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('vacancies', function (Blueprint $table) {
-            $table->id();  
+            $table->id();
             $table->string('title');
             $table->text('description');
             $table->decimal('salary_min', 8, 2)->nullable();
             $table->decimal('salary_max', 8, 2)->nullable();
             $table->json('requirements')->nullable();
             $table->json('benefits')->nullable();
-            $table->enum('status',['open','closed'])->default('open');
+            $table->enum('status', ['open', 'closed'])->default('open');
             $table->timestamps();
-
 
             $table->foreignId('company_id')->references('id')->on('companies')->cascadeOnDelete();
         });
